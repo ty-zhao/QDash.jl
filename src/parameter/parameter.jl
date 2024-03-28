203b07c248c49c0d88921aee1747ecf704c9d49b
Tongyu Zhao <Tongyu.Zhao@colorado.edu>
2024-03-28 16:56:02 -0600

    remove unused function

--- src/parameter/parameter.jl
+++ src/parameter/parameter.jl
@@ -3,17 +3,13 @@ using Unitful
 abstract type Parameter end
 
 """
-	GenericParameter(name::String, label::String, unit::Unitful.Units)
+    GenericParameter(name::String, label::String, unit::Unitful.Units)
 A generic parameter with a name, label, and unit.
 """
 mutable struct GenericParameter <: Parameter
-	name::String
-	label::String
-	unit::Unitful.Units
+    name::String
+    label::String
+    unit::Unitful.Units
 end
 
 GenericParameter() = GenericParameter("", "", NoUnits)
-
-function getfield()
-	
-end
