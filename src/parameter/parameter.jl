502df9a3a1f738b632b2324b5f4ba45669662908
Tongyu Zhao <Tongyu.Zhao@colorado.edu>
2024-03-28 16:22:08 -0600

    update docs

--- src/parameter/parameter.jl
+++ src/parameter/parameter.jl
@@ -2,6 +2,10 @@ using Unitful
 
 abstract type Parameter end
 
+"""
+	GenericParameter(name::String, label::String, unit::Unitful.Units)
+A generic parameter with a name, label, and unit.
+"""
 mutable struct GenericParameter <: Parameter
 	name::String
 	label::String
