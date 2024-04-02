7d35e2cac87dcb2a748a2e0e771c59a063e62d59
Tongyu Zhao <ty.zhao.work@gmail.com>
2024-04-01 21:53:46 -0600

    fix function call type

--- src/qobject/qobject.jl
+++ src/qobject/qobject.jl
@@ -4,6 +4,6 @@ Qobject is the base class of all QDash objects.
 """
 abstract type Qobject end
 
-load_metadata!(q::Qobject, metadata::Dict{String, Any}) = merge!(q.metadata, metadata)
+load_metadata!(q::Qobject, metadata::Dict{String, <:Any}) = merge!(q.metadata, metadata)
 
 snapshot(q::Qobject) = deepcopy(q.metadata)
