f811ae24007939ef65095c8a9795582b9444cddf
Tongyu Zhao <tongyu.zhao@colorado.edu>
2024-04-23 19:22:35 -0600

    docs: remove `Qobjects` docstring for now

--- src/qobject.jl
+++ src/qobject.jl
@@ -1,7 +1,3 @@
-"""
-    Qobject
-Qobject is the base class of all QDash objects.
-"""
 abstract type Qobject end
 
 load_metadata!(q::Qobject, metadata::Dict{String, <:Any}) = merge!(q.metadata, metadata)
