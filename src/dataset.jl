89e7b2c65a972590326cb6c7aa2f28f37fdb3eb2
Tongyu Zhao <Tongyu.Zhao@colorado.edu>
2024-04-26 14:36:57 -0600

    feat: add `filter_metadata` to help initialize dataset

--- src/dataset.jl
+++ src/dataset.jl
@@ -3,15 +3,16 @@ function initialize_dataset(
     setpoints     :: Vector,
     gettable_pars :: Vector{<:Parameter},
 )
+    fields_to_log = ["name", "label", "unit"]
     axlist = Tuple(
-        Dim{Symbol("x$i")}(setpoints[i], metadata=delete!(snapshot(settable), "ts"))
+        Dim{Symbol("x$i")}(setpoints[i]; metadata=filter_metadata(settable, fields_to_log))
         for (i, settable) in enumerate(settable_pars)
     )
     axshape = Tuple(length(ax) for ax in axlist)
 
     varnames = Tuple(Symbol("y$i") for i in eachindex(gettable_pars))
     varlist = Tuple(
-        YAXArray(axlist, fill(NaN, axshape), delete!(snapshot(gettable), "ts"))
+        YAXArray(axlist, fill(NaN, axshape), filter_metadata(gettable, fields_to_log))
         for (i, gettable) in enumerate(gettable_pars)
     )
 
@@ -22,4 +23,10 @@ function gen_tuid(;time::TimeType=now())
     timestring = string(Dates.format(time, "yyyymmdd-HHMMSS-sss"))
 
     return string(timestring, "-", string(uuid4())[1:6])
+end
+
+function filter_metadata(p::AbstractParameter, fields::Vector{String})
+    metadata = snapshot(p)
+
+    return Dict(f => metadata[f] for f in fields if haskey(metadata, f))
 end
