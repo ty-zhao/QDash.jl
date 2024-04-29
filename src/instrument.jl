c7ef75fc2b3be4ee3cb495e85629e6e0f86226c6
Tongyu Zhao <tongyu.zhao@colorado.edu>
2024-04-29 11:08:16 -0600

    refactor: remove `Symbol` from parameter tables

--- src/instrument.jl
+++ src/instrument.jl
@@ -34,12 +34,12 @@ function Base.getproperty(i::AbstractInstrument, s::Symbol)
     error("Unsupported property: $s")
 end
 
-function addparameter!(i::AbstractInstrument, name::Symbol, p::AbstractParameter)
-    if name in keys(i.parameters)
+function addparameter!(i::AbstractInstrument, p::AbstractParameter)
+    if Symbol(p.name) in keys(i.parameters)
         error("Parameter $name already exists in instrument")
     end
 
-    i.parameters[name] = p
+    i.parameters[Symbol(p.name)] = p
 
     return nothing
 end
@@ -49,9 +49,9 @@ function Base.show(io::IO, ::MIME"text/plain", d::Dict{Symbol, Parameter})
     println(io)
 
     if length(d) == 0
-        column_widths = (6, 4, 5, 5, 4)
+        column_widths = (4, 5, 4, 5)
         print_horizontal_line(io, column_widths, ('╭', '─', '┬', '╮'))
-        printstyled(io, "│Symbol │Name │Label │Value │Unit │"; bold=true)
+        printstyled(io, "│Name │Label │Unit │Value │"; bold=true)
         println(io)
         print_horizontal_line(io, column_widths, ('├', '─', '┼', '┤'))
         print_horizontal_line(io, column_widths, ('╰', '─', '┴', '╯'))
@@ -68,25 +68,24 @@ function Base.show(io::IO, ::MIME"text/plain", d::Dict{Symbol, Parameter})
         ) for v in values(d)
     ]
 
-    symbol_width = max(length("Symbol"), maximum([length(string(k)) for k in keys(d)])) + 1
     name_width   = max(length("Name"), maximum([length(v.name) for v in values(d)]))
     label_width  = max(length("Label"), maximum([length(v.label) for v in values(d)]))
     value_width  = max(length("Value"), maximum(length.(value_strings)))
     unit_width   = max(length("Unit"), maximum([length(string(v.unit)) for v in values(d)]))
-    column_widths = (symbol_width, name_width, label_width, value_width, unit_width)
+    column_widths = (name_width, label_width, unit_width, value_width)
 
     print_horizontal_line(io, column_widths, ('╭', '─', '┬', '╮'))
 
-    printstyled(io, "│", rpad("Symbol", symbol_width, " "), " │"; bold=true)
-    printstyled(io, rpad("Name", name_width, " "), " │"; bold=true)
+    printstyled(io, "│", rpad("Name", name_width, " "), " │"; bold=true)
     printstyled(io, rpad("Label", label_width, " "), " │"; bold=true)
-    printstyled(io, rpad("Value", value_width, " "), " │"; bold=true)
     printstyled(io, rpad("Unit", unit_width, " "), " │"; bold=true)
+    printstyled(io, rpad("Value", value_width, " "), " │"; bold=true)
     println(io)
 
     print_parameters(io, d, column_widths)
     print_horizontal_line(io, column_widths, ('╰', '─', '┴', '╯'))
 
+    return nothing
 end
 
 function print_parameters(io::IO, d::Dict{Symbol, Parameter}, column_width::Tuple)
@@ -100,11 +99,10 @@ function print_parameters(io::IO, d::Dict{Symbol, Parameter}, column_width::Tupl
             sizehint=0
         )
 
-        print(io, "│:", rpad(k, column_width[1]-1, " "), " │")
-        print(io, rpad(v.name, column_width[2], " "), " │")
-        print(io, rpad(v.label, column_width[3], " "), " │")
+        print(io, "│", rpad(v.name, column_width[1], " "), " │")
+        print(io, rpad(v.label, column_width[2], " "), " │")
+        print(io, rpad(string(v.unit), column_width[3], " "), " │")
         print(io, rpad(value_string, column_width[4], " "), " │")
-        print(io, rpad(string(v.unit), column_width[5], " "), " │")
         println(io)
     end
 
