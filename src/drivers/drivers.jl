496520a16b49d37b8f59ecfa9a4798813efb830f
Tongyu Zhao <ty.zhao.work@gmail.com>
2024-05-06 12:00:10 -0600

    refactor: bring visa utility functions back and update test driver

--- src/drivers/drivers.jl
+++ src/drivers/drivers.jl
@@ -3,34 +3,21 @@ function AgilentN5183M(;
     address :: String,
     label   :: String = "label",
 )
-    model = :AgilentN5183M
-    timestamp = string(now())
-    metadata = Dict(
-        "model" => model,
-        "name"  => name,
-        "label" => label,
-        "ts"    => timestamp,
-        "address" => address,
-    )
-
     parameters = Dict{Symbol, Parameter}()
     parameters[:frequency] = Parameter(;
         name="frequency",
         label="Frequency",
         value=0.0,
         unit=u"Hz",
-        instrument = model,
+        instrument=:AgilentN5183M,
     )
 
-    return Instrument{model}(
-        name,
-        address,
-        label,
-        timestamp,
-        0,
-        false,
-        UInt32(1024),
-        metadata,
-        parameters,
+    return Instrument(
+        Val(:VISA);
+        model=:AgilentN5183M,
+        name=name,
+        address=address,
+        label=label,
+        parameters=parameters,
     )
 end
